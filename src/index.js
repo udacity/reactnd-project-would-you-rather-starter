037b3bbc66a17c2d891ad4374d78bcce4fb152d4
MarcusNTuttle <marcusntuttle@gmail.com>
2018-08-04 15:01:22 -0700

    Add initial store, reducer, and middleware

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
 
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
-<App />, 
+  <Provider store={store}>
+    <App />
+  </Provider>,
 document.getElementById('root'));
 
