ba0bac3286a62905f04edbb5f9580fe84439d61c
MarcusNTuttle <marcusntuttle@gmail.com>
2018-08-04 13:56:50 -0700

    Create base and file structure, update gitignore

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello There! 
+      </div>
+    );
+  }
+}
+
+export default App;
