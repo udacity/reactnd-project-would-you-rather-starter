f0fc25e667b984ac4e77449fc7524a2b57820789
MarcusNTuttle <marcusntuttle@gmail.com>
2018-08-04 18:45:21 -0700

    initial commit of App and Home Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 
+import Home from './Home'
+import Login from './Login'
+import Page_404 from './Page_404';
+
 
 class App extends Component {
+  
   componentDidMount() {
     this.props.dispatch(handleInitialData())
   }
 
   render() {
-    return (
-      <div>
-        Hello There! 
-      </div>
-    );
+    const { authedUser} = this.props;
+
+    if (authedUser === null) {
+      return (
+        <Login />
+      )
+    } else {
+      return (
+        <Router>
+          <Fragment>
+            <div className='container'>
+              <Switch>
+                <Route path='/' exact component={Home} />
+                <Route component={Page_404} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      )
+    }
+
+  }
+}
+
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser,
   }
 }
 
 
-export default connect()(App)
+export default connect(mapStateToProps)(App)
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className='center'>Home</h3>
+      </div>
+    )
+  }
+}
+
+
+export default Home
