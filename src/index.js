71edfed3c2eacc94656bf85563294b13cc1eb10a
MarcusNTuttle <marcusntuttle@gmail.com>
2018-08-04 18:43:05 -0700

    Add semantic-ui package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
+
+/* styling */
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+
 
 const store = createStore(reducer, middleware)
 
