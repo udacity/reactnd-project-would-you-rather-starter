175e39b8dcd41f916c51c05dec13de88b9db2752
MarcusNTuttle <marcusntuttle@gmail.com>
2018-08-08 03:46:08 -0700

    refactor: authedUser focuses on userID instead of Obj from store

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,8 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 
-export function setAuthedUser(authedUser) {
+export function setAuthedUser(id) {
   return {
     type: SET_AUTHED_USER,
-    authedUser,
+    id,
   }
 }
--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -3,7 +3,8 @@ import { SET_AUTHED_USER } from '../actions/authedUser'
 export default function authedUser(state = null, action) {
   switch (action.type) {
     case SET_AUTHED_USER:
-      return action.authedUser
+      return action.id
+
     default:
       return state
   }
