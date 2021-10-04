5567c5ba8f085c11c2e0c685cb4d3f6f8e730356
Adedeji Adelanwa <adedejiadelanwa@gmail.com>
2021-10-04 08:54:30 +0100

    fix: Complete navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import styled from "styled-components";
 
 const NavWrapper = styled.nav`
@@ -12,7 +12,7 @@ const NavWrapper = styled.nav`
   align-items: center;
   justify-content: flex-end;
   div.links {
-    width: 30%;
+    width: 40%;
     display: flex;
     justify-content: space-evenly;
   }
@@ -22,28 +22,46 @@ const NavWrapper = styled.nav`
     color: inherit;
     text-decoration: none;
   }
+  .user-profile {
+    height: 50px;
+    width: 10%;
+    display: flex;
+    align-items: center;
+    img {
+      border-radius: 50%;
+      margin-right: 5px;
+    }
+  }
   .logout {
     margin-left: 10px;
     margin-right: 10px;
+    font-size: smaller;
+  }
+  .curent {
+    padding: 5px 10px;
+    background-color: #fff;
   }
 `;
 
 function Navbar() {
   const authedUser = useSelector((state) => state.authedUser.authedUser);
-  useEffect(() => {
-    console.log(authedUser);
-  });
+
   return (
     <NavWrapper>
       <div className="links">
-        <Link to="/home">Home</Link>
-        <Link to="/leaderboard">Leaderbord</Link>
-        <Link to="/newpoll">Create Poll</Link>
+        <NavLink activeClassName="current" to="/home">
+          Home
+        </NavLink>
+        <NavLink activeClassName="current" to="/leaderboard">
+          Leaderbord
+        </NavLink>
+        <NavLink activeClassName="current" to="/newpoll">
+          Create Poll
+        </NavLink>
       </div>
       <div className="user-profile">
-        <div>
-          <img src={authedUser.avatarURL} alt="user pic" />
-        </div>
+        <img width="40" height="40" src={authedUser.avatarURL} alt="user pic" />
+
         <p>{authedUser.id}</p>
       </div>
       <Link className="logout" to="/signin">
